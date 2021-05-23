2498bb4934bf916f4803d6807dfeef1cd08329d8
jwajungmin <jmjwa7@gmail.com>
2021-05-23 23:12:10 +0900

    commit message
    Signed-off-by: jwajungmin <jmjwa7@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# toy-shell
+# toy shell
+
+20163421
+
+jwajungmin
+
+This code is compiled in WSL2 (Ubuntu).
+
+It is in Script language and C language.
+
+You can see with this program User and Host name, Hello world, clock, cwd, CLA, DISK condition,
+and some of command, exit, name function.
+
+Firstable, you have to type "gcc shell.c", "gcc -o shell shell.c " and "bash toyshell" for start shell. Then you can see Hello world, clock,
+cwd, DISK condition. But if you want to command line argument in start page, you should type 
+bash shell and more words like "bash shell a b c d".
+
+After that, there will be "MyShell $". You can type there some command like /bin/ls, then you
+can see argv, argc, envp, and contents by command.
+
+Also you can see username and hostname by typing "name", and in the same way you can quit
+the shell by typing "exit".
+
+Thank you!
--- shell.c
+++ shell.c
@@ -2,60 +2,87 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <pwd.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 #define MAX_LEN_LINE    100
+#define LEN_HOSTNAME	30
 
-int main(void)
+int main(int argc, char* argv[], char* envp[])
 {
     char command[MAX_LEN_LINE];
-    char *args[] = {command, NULL};
+    char* args[] = { command, NULL };
     int ret, status;
     pid_t pid, cpid;
-    
+
     while (true) {
-        char *s;
+        char* s;
         int len;
-        
+
         printf("MyShell $ ");
         s = fgets(command, MAX_LEN_LINE, stdin);
         if (s == NULL) {
             fprintf(stderr, "fgets failed\n");
             exit(1);
         }
-        
+
+        int i;
+        printf("----> Number of arguments = %d\n", argc);
+        for (i = 0; i < argc; i++) {
+            printf("----> argv[%d] = \"%s\"\n", i, argv[i]);
+        }
+        for (i = 0; envp[i] != NULL; i++) {
+            printf("----> envp[%d] = \"%s\"\n", i, envp[i]);
+        }
+
         len = strlen(command);
         printf("%d\n", len);
         if (command[len - 1] == '\n') {
-            command[len - 1] = '\0'; 
+            command[len - 1] = '\0';
         }
-        
+
         printf("[%s]\n", command);
-      
+
+        if (!strcmp(command, "exit")) {
+            return 3;
+        }
+
+        if (!strcmp(command, "name")) {
+
+            char hostname[LEN_HOSTNAME + 1];
+            memset(hostname, 0x00, sizeof(hostname));
+            printf("username: %s\n", getpwuid(getuid())->pw_name);
+
+            gethostname(hostname, LEN_HOSTNAME);
+            printf("hostname: %s\n", hostname);
+        }
+
         pid = fork();
         if (pid < 0) {
             fprintf(stderr, "fork failed\n");
             exit(1);
-        } 
+        }
         if (pid != 0) {  /* parent */
             cpid = waitpid(pid, &status, 0);
             if (cpid != pid) {
-                fprintf(stderr, "waitpid failed\n");        
+                fprintf(stderr, "waitpid failed\n");
             }
             printf("Child process terminated\n");
             if (WIFEXITED(status)) {
-                printf("Exit status is %d\n", WEXITSTATUS(status)); 
+                printf("Exit status is %d\n", WEXITSTATUS(status));
             }
         }
         else {  /* child */
             ret = execve(args[0], args, NULL);
             if (ret < 0) {
-                fprintf(stderr, "execve failed\n");   
+                fprintf(stderr, "execve failed\n");
                 return 1;
             }
-        } 
+        }
+
     }
     return 0;
 }
+
